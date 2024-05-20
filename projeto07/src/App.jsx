c7f4205a6ed83b42ca9e28061644ac3860a1297a
tamiressil <tamiressilvabezerra05@gmail.com>
2024-05-20 11:45:37 -0300

    first commit

---
+++ projeto07/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BuscarFilmes from './Componentes/Filmes'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <BuscarFilmes/>
+    </div>
+  )
+}
